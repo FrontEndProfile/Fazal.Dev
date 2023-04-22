2f8a9d901d5f3c90c7ee486737fda7370754ca22
Fazal <102220108+FrontEndProfile@users.noreply.github.com>
2023-04-22 09:53:43 -0700

    fix ui code img and start resume page

--- src/components/Resume/ResumeComponent.js
+++ src/components/Resume/ResumeComponent.js
@@ -6,7 +6,7 @@ import resumeEducation from '../../assets/images/resumeEducation.svg'
 const ResumeComponent = () => {
     return (
         <>
-            <section className='mainPage pb-0 primaryFont'>
+            <section className='mainPage pb-0 primaryFont ResumePage'>
                 <div className='container-fluid g-0 text-start primaryFont'>
                     <div className='row'>
                         <div className='col'>
@@ -17,8 +17,15 @@ const ResumeComponent = () => {
                         <div className='col-md-4'>
                             <div className='columnMain'>
                                 <div className='columnMainHead'>
-                                    <span><img src={resumeEducation} alt=''/></span>
+                                    <span><img src={resumeEducation} alt='' /></span>
                                     <span>Education</span>
+                                </div>
+
+                                {/* CARD'S  */}
+                                <div class="ResumeCard">
+                                    <span class="CardDate">2021-2023</span>
+                                    <h3 class="CardSkill"> Ph.D in Horriblensess</h3>
+                                    <p class="CardInfo">ABC University, Los Angeles, CA</p>
                                 </div>
                             </div>
                         </div>
